6baf40efc6b6579b1f01f5efd2827d8975a86116
David Rajchenbach-Teller <david.teller@univ-orleans.fr>
2016-02-26 17:37:42 +0100

    WIP: Simulator implemented put_value

--- examples/simulator.rs
+++ examples/simulator.rs
@@ -4,8 +4,6 @@
 extern crate docopt;
 extern crate serde;
 extern crate serde_json;
-#[macro_use]
-extern crate lazy_static;
 
 extern crate fxbox_thinkerbell;
 extern crate fxbox_taxonomy;
@@ -39,8 +37,9 @@ Usage: simulator [options]...
        simulator --help
 
 -h, --help            Show this message.
--r, --ruleset <path>  Load rules from a file.
--o, --output <path>   Load definition of an output device.
+-r, --ruleset <path>  Load decision rules from a file.
+-d, --devices <path>  Load devices from a file.
+-s, --slowdown <num>  Duration of each tick, in ms. Default: 1ms. 
 ";
 
 #[derive(Default, Serialize, Deserialize)]
@@ -69,19 +68,30 @@ enum Op {
     AddNodes(Vec<Node>),
     AddInputs(Vec<Service<Input>>),
     AddOutputs(Vec<Service<Output>>),
-    AddWatch{options: Vec<WatchOptions>, cb: Box<FnMut(WatchEvent) + Send + 'static>},
+    AddWatch{options: Vec<WatchOptions>, cb: Box<Fn(WatchEvent) + Send + 'static>},
 }
 
 enum Update {
     Put { id: ServiceId, value: Value, result: Result<(), String> },
+    Inject { id: ServiceId, value: Value, result: Result<(), String> },
     Done(String),
+}
+
+struct InputWithState {
+    input: Service<Input>,
+    state: Option<Value>,
+}
+impl InputWithState {
+    fn set_state(&mut self, val: Value) {
+        self.state = Some(val);
+    }
 }
 
 struct APIBackEnd {
     nodes: HashMap<NodeId, Node>,
-    inputs: HashMap<ServiceId, (Service<Input>, Option<Value>)>,
+    inputs: HashMap<ServiceId, InputWithState>,
     outputs: HashMap<ServiceId, Service<Output>>,
-    watchers: Vec<(WatchOptions, Arc<Box<FnMut(WatchEvent)>>)>,
+    watchers: Vec<(WatchOptions, Arc<Box<Fn(WatchEvent)>>)>,
     post_updates: Arc<Fn(Update)>
 }
 impl APIBackEnd {
@@ -96,84 +106,63 @@ impl APIBackEnd {
         }
     }
     
-    fn add_nodes(&mut self, nodes: Vec<Node>) -> Result<(), ()> {
+    fn add_nodes(&mut self, nodes: Vec<Node>) {
         for node in nodes {
             let previous = self.nodes.insert(node.id.clone(), node);
-            if previous.is_none() {
-                return Err(());
+            if previous.is_some() {
+                assert!(previous.is_none());
             }
         }
-        Ok(())
-        // FIXME: In a real implementation, this should update all NodeRequest
+        // In a real implementation, this should update all NodeRequest
     }
-    fn add_inputs(&mut self, inputs: Vec<Service<Input>>) -> Result<(), ()> {
+    fn add_inputs(&mut self, inputs: Vec<Service<Input>>) {
         for input in inputs {
-            let previous = self.inputs.insert(input.id.clone(), (input, None));
-            if previous.is_none() {
-                return Err(());
-            }
+            let previous = self.inputs.insert(
+                input.id.clone(),
+                InputWithState {
+                    input:input,
+                    state: None
+                });
+            assert!(previous.is_none());
         }
-        Ok(())
-        // FIXME: In a real implementation, this should update all InputRequests
+        // In a real implementation, this should update all InputRequests
     }
-    fn add_outputs(&mut self, outputs: Vec<Service<Output>>) -> Result<(), ()> {
+    fn add_outputs(&mut self, outputs: Vec<Service<Output>>)  {
         for output in outputs {
             let previous = self.outputs.insert(output.id.clone(), output);
-            if previous.is_none() {
-                return Err(());
-            }
+            assert!(previous.is_none());
         }
-        Ok(())
-        // FIXME: In a real implementation, this should update all OutputRequests
+        // In a real implementation, this should update all OutputRequests
     }
 
-    fn add_watch(&mut self, options: Vec<WatchOptions>, cb: Box<FnMut(WatchEvent)>) -> Result<(), ()> {
+    fn add_watch(&mut self, options: Vec<WatchOptions>, cb: Box<Fn(WatchEvent)>) {
         let cb = Arc::new(cb);
         for opt in options {
             self.watchers.push((opt, cb.clone()));
         }
-        Ok(())
     }
 
-    fn put_value(&mut self, filters: Vec<OutputRequest>, value: Value) {
-        // Very suboptimal implementation.
-        let outputs = self.outputs.values().filter(|output| {
-            for request in &filters {
-                if !request.id.matches(&output.id) {
-                    return false;
-                }
-                if !request.parent.matches(&output.node) {
-                    return false;
-                }
-                if !request.kind.matches(&output.mechanism.kind) {
-                    return false;
-                }
-                if let Some(Period {ref min, ref max}) = request.push {
-                    if let Some(ref current) = output.mechanism.push {
-                        if let Some(ref min) = *min {
-                            if min > current {
-                                return false;
-                            }
-                        }
-                        if let Some(ref max) = *max {
-                            if max < current {
-                                return false;
-                            }
-                        }
-                    } else {
-                        return false;
-                    }
-                }
-                for tag in &request.tags {
-                    if output.tags.iter().find(|x| *x == tag).is_none() {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            // No filters? Then nothing matches.
-            return false;
+    fn inject_input_value(&mut self, id: ServiceId, value: Value) {
+        let mut input = self.inputs.get_mut(&id).unwrap();
+        input.set_state(value.clone());
+
+        // The list of watchers watching for new values on this input.
+        let watchers = self.watchers.iter().filter(|&&(ref options, _)| {
+            options.should_watch_values &&
+                options.source.matches(&input.input)
         });
+        for mut watcher in watchers {
+            watcher.1(WatchEvent::Value {
+                from: id.clone(),
+                value: value.clone()
+            });
+        }
+    }
+
+    fn put_value(&mut self, requests: Vec<OutputRequest>, value: Value) {
+        // Very suboptimal implementation.
+        let outputs = self.outputs.values().filter(|output|
+            requests.iter().find(|request| request.matches(output)).is_some());
         for output in outputs {
             let result =
                 if value.get_type() == output.mechanism.kind.get_type() {
@@ -221,10 +210,10 @@ impl APIFrontEnd {
             for msg in rx.iter() {
                 use Op::*;
                 match msg {
-                    AddNodes(vec) => api.add_nodes(vec).unwrap(),
-                    AddInputs(vec) => api.add_inputs(vec).unwrap(),
-                    AddOutputs(vec) => api.add_outputs(vec).unwrap(),
-                    AddWatch{options, cb} => api.add_watch(options, cb).unwrap()
+                    AddNodes(vec) => api.add_nodes(vec),
+                    AddInputs(vec) => api.add_inputs(vec),
+                    AddOutputs(vec) => api.add_outputs(vec),
+                    AddWatch{options, cb} => api.add_watch(options, cb),
                 }
             }
         });
@@ -273,7 +262,7 @@ impl API for APIFrontEnd {
     fn put_service_value(&self, _: &Vec<OutputRequest>, _: Value) -> Vec<(ServiceId, Result<(), APIError>)> {
         unimplemented!()
     }
-    fn register_service_watch(&self, options: Vec<WatchOptions>, cb: Box<FnMut(WatchEvent) + Send + 'static>) -> Self::WatchGuard {
+    fn register_service_watch(&self, options: Vec<WatchOptions>, cb: Box<Fn(WatchEvent) + Send + 'static>) -> Self::WatchGuard {
         self.tx.send(Op::AddWatch {
             options: options,
             cb: cb
@@ -282,196 +271,6 @@ impl API for APIFrontEnd {
     }
 
 }
-
-/*
-/// An implementation of DevEnv for the purpose of unit testing.
-struct State {
-    states: HashMap</*device*/String, HashMap</*capability*/String, HashMap<String, Value>> >,
-    inputs: Vec<(String, String)>,
-    outputs: Vec<(String, String)>
-}
-impl State {
-    fn new() -> State {
-        State {
-            states: HashMap::new(),
-            inputs: Vec::new(),
-            outputs: Vec::new(),
-        }
-    }
-    fn reset(&mut self) {
-        self.states.clear();
-    }
-}
-
-lazy_static! {
-    static ref STATE: Mutex<State> = Mutex::new(State::new());
-}
-
-struct TestEnv;
-
-impl TestEnv {
-    fn reset() {
-        STATE.lock().unwrap().reset();
-    }
-
-    fn get_state(device: &String, cap: &String) -> Option<HashMap<String, Value>> {
-        println!("[IN] Fetching the state of input device {}, service {}", device, cap);
-        let states = &STATE.lock().unwrap().states;
-        states.get(device).and_then(|per_device| {
-            per_device.get(cap).cloned()
-        })
-    }
-
-    fn set_state(device: &String, cap: &String, state: HashMap<String, Value>) {
-        println!("[OUT] Setting the state of output device {}, service {}", device, cap);
-        let mut states = &mut STATE.lock().unwrap().states;
-        if !states.contains_key(device) {
-            states.insert(device.clone(), HashMap::new());
-        }
-        let per_device = states.get_mut(device).unwrap();
-        per_device.insert(cap.clone(), state);
-    }
-}
-
-impl DevEnv for TestEnv {
-    type DeviceKind = String;
-    type Device = String;
-    type InputCapability = String;
-    type OutputCapability = String;
-}
-
-impl ExecutableDevEnv for TestEnv {
-        type Watcher = TestWatcher;
-
-    fn get_watcher() -> Self::Watcher {
-        Self::Watcher::new()
-    }
-
-    fn get_device_kind(key: &String) -> Option<String> {
-        // A set of well-known device kinds
-        for s in vec!["clock", "display device", "kind 3"] {
-            if s == key {
-                return Some(key.clone());
-            }
-        }
-        None
-    }
-
-    fn get_device(key: &String) -> Option<String> {
-        // A set of well-known devices
-        println!("Getting device {}", key);
-        for s in vec!["built-in clock", "built-in display 1", "built-in display 2"] {
-            if s == key {
-                return Some(key.clone());
-            }
-        }
-        None
-    }
-
-    fn get_input_capability(key: &String) -> Option<String> {
-        // A set of well-known inputs
-        for s in vec!["ticks", "input 2:string", "input 3: bool"] {
-            if s == key {
-                println!("Getting input capability {}", key);
-                return Some(key.clone());
-            }
-        }
-        None
-    }
-
-    fn get_output_capability(key: &String) -> Option<String> {
-        for s in vec!["show", "output 2", "output 3"] {
-            if s == key {
-                println!("Getting output capability {}", key);
-                return Some(key.clone());
-            }
-        }
-        None
-    }
-
-    fn send(device: &Self::Device, cap: &Self::OutputCapability, value: &HashMap<String, Value>) {
-        TestEnv::set_state(device, cap, value.clone());
-    }
-}
-
-/// A mock watcher that informs clients with new values regularly.
-
-enum TestWatcherMsg {
-    Stop,
-    Insert((String, String), Box<Fn(Value) + Send>)
-}
-
-struct TestWatcher {
-    tx: Sender<TestWatcherMsg>,
-}
-
-impl TestWatcher {
-    fn new() -> Self {
-        use TestWatcherMsg::*;
-        let (tx, rx) = channel();
-
-        // FIXME: This should be replaced by manual entry on the REPL
-        
-        thread::spawn(move || {
-            let mut watchers = HashMap::new();
-            let mut ticks = 0;
-
-            let clock_key = ("built-in clock".to_owned(), "ticks".to_owned());
-            loop {
-                ticks += 1;
-                if let Ok(msg) = rx.try_recv() {
-                    match msg {
-                        Stop => {
-                            return;
-                        },
-                        Insert(k, cb) => {
-                            watchers.insert(k, cb);
-                        }
-                    }
-                } else {
-                    thread::sleep(std::time::Duration::new(1, 0));
-
-                    let clock_key = clock_key.clone();
-                    let ticks = ticks.clone();
-                    match watchers.get(&clock_key) {
-                        None => {},
-                        Some(ref watcher) => {
-                            let val = Value::Duration(Duration::new(ticks, 0));
-                            println!("[WATCH] State of device {} service {} has reached {:?}", &clock_key.0, &clock_key.1, &val);
-                            watcher(val);
-                        }
-                    }
-                }
-            }
-        });
-        TestWatcher {
-            tx: tx,
-        }
-    }
-}
-
-impl Watcher for TestWatcher {
-    type Witness = ();
-    type Device = String;
-    type InputCapability = String;
-
-    fn add<F>(&mut self,
-              device: &Self::Device,
-              input: &Self::InputCapability,
-              _condition: &Range,
-              cb: F) -> Self::Witness where F:Fn(Value) + Send + 'static
-{
-        self.tx.send(TestWatcherMsg::Insert((device.clone(), input.clone()), Box::new(cb))).unwrap();
-        ()
-    }
-}
-
-impl Drop for TestWatcher {
-    fn drop(&mut self) {
-        self.tx.send(TestWatcherMsg::Stop).unwrap();
-    }
-}
-*/
 fn main () {
     let env = TestEnv::new(|_|{});
 
